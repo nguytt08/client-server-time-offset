a7504416c29f1aee95b5efa47ee26c7d9ef7ba01
Tin <tinytheyfon8@gmail.com>
2017-03-28 03:05:11 -0500

    Completed and cleaned up code

--- client/time.js
+++ client/time.js
@@ -2,25 +2,18 @@ var socket = io.connect('http://localhost:4000');
 var currentTime = Math.floor(new Date().getTime());
 
 
-// socket.connect();
-
+//Initial Socket connection, after connection, sends current time off to server
 socket.on('connect', function() {
-  console.log('Client has connected to the server');
+  // console.log('Client has connected to the server');
   socket.emit('clientTime', currentTime);
 });
 
+//Listens for event then appends response to page
 socket.on('timeDifference', function(data) {
-  console.log('Received a message from the server', data);
-  $('.time').append(data + ' milliseconds');
+  // console.log('Received a message from the server', data);
+  $('.time').append(Math.abs(data) + ' milliseconds');
 });
-
 
 socket.on('disconnect', function() {
-  console.log('client has disconnected');
+  console.log('Client has disconnected');
 });
-
-//Current time in milliseconds via Unix timestamp
-
-
-// var currentTime = Date.now()
-console.log(currentTime);
--- server/index.js
+++ server/index.js
@@ -2,45 +2,30 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var server = require('http').createServer(app);
-var moment = require('moment');
 var io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, '../client')));
 
 
-var serverTime = function () {
-  var currentTime = Math.floor((new Date()).getTime());
-  console.log(currentTime);
-}
 app.get('/', function(req, res) {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
 
-serverTime();
 
-// var now = moment.;
-// console.log('moment time is:', now);
-
+//Once connection is established, will listen for clientTime event and then emit the result of timeDifference function call
 io.on('connection', function(socket) {
-  console.log('socket workinggggg');
   socket.on('clientTime', function(clientTime) {
-    // console.log('client time is', clientTime);
     socket.emit('timeDifference', timeDifference(clientTime));
-
-  })
+  });
 });
 
-// .then(socket.emit('timeDifference', timeDifference(clientTime));
-
+//Simple difference calculator
 var timeDifference = function(clientTime) {
-  var serverTime = Math.floor((new Date()).getTime());
+  let serverTime = Math.floor((new Date()).getTime());
   let difference = clientTime - serverTime;
-  console.log(difference);
-
   return difference;
 }
 
-
-
-console.log('App is listening on 4000');
-server.listen(4000);
+var port = process.env.PORT || 4000
+console.log('App is listening on', port);
+server.listen(port);
