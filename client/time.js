7716daacc0b2df61175fe960a6e3502f3d3e8288
Tin <tinytheyfon8@gmail.com>
2017-03-28 01:12:58 -0500

    Completed functionality, need to deploy and test

--- client/time.js
+++ client/time.js
@@ -1,3 +1,26 @@
-var currentTime = Math.floor((new Date()).getTime() / 1000);
+var socket = io.connect('http://localhost:4000');
+var currentTime = Math.floor((new Date()).getTime());
 
+
+// socket.connect();
+
+socket.on('connect', function() {
+  console.log('Client has connected to the server');
+  socket.emit('clientTime', currentTime);
+});
+
+socket.on('timeDifference', function(data) {
+  console.log('Received a message from the server', data);
+  $('.time').append(data + ' milliseconds');
+});
+
+
+socket.on('disconnect', function() {
+  console.log('client has disconnected');
+});
+
+//Current time in milliseconds via Unix timestamp
+
+
+// var currentTime = Date.now()
 console.log(currentTime);
--- server/index.js
+++ server/index.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, '../client')));
 
-// app.get('/', function(req, res) {
-//   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
-// });
+
+var serverTime = function () {
+  var currentTime = Math.floor((new Date()).getTime());
+  console.log(currentTime);
+}
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+serverTime();
+
+io.on('connection', function(socket) {
+  console.log('socket workinggggg');
+  socket.on('clientTime', function(clientTime) {
+    // console.log('client time is', clientTime);
+    socket.emit('timeDifference', timeDifference(clientTime));
+
+  })
+});
+
+// .then(socket.emit('timeDifference', timeDifference(clientTime));
+
+var timeDifference = function(clientTime) {
+  var serverTime = Math.floor((new Date()).getTime());
+  let difference = clientTime - serverTime;
+  console.log(difference);
+
+  return difference;
+}
+
 
 
 console.log('App is listening on 4000');
-app.listen(4000);
+server.listen(4000);
